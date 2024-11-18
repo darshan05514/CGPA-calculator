58831124a1144d41939e97f831da20fd4176160a
ParikshitKamath05 <kparikshit915@gmail.com>
2024-11-18 15:50:36 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Hello
+{
+    public static void main(String args[])
+    {
+    Scanner scan=new Scanner(System.in);
+    int a=scan.nextInt();
+    System.out.println("a");
+    
+    String b=scan.next();
+    System.out.println("b");
+    
+    float c=scan.nextFloat();
+    System.out.println("c");
+    
+    double d=scan.nextDouble();
+    System.out.println("d");
+    }
+}
